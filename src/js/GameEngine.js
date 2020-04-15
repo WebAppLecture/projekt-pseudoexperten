f40db8d2fedfe7cbf37268789abeb7268789c787
Sandro <sandro.michl@web.de>
2020-04-15 16:04:28 +0200

    Read_Name1

--- src/js/GameEngine.js
+++ src/js/GameEngine.js
@@ -34,7 +34,7 @@ export class GameEngine{
         this.gameLoop();
     }
     
-    gameLoop() {  
+    gameLoop() {
         if(this.game !== undefined) {
             requestAnimationFrame(this.gameLoop.bind(this));  
             this.renderContext.clearRect(0,0,this.screen.width, this.screen.height);
@@ -54,7 +54,22 @@ export class GameEngine{
     }
       
     savePlayersName(){
-        console.log("savePlayersName");
+        this.msg = document.getElementById("name").value;
+        
+        let element = document.getElementById('screen');
+ 
+        if(element.getContext) {
+          let context = element.getContext('2d'),
+          text = "Hallo " + this.msg + ", lass uns beginnen!";
+          context.clearRect(0, 0, element.width, element.height);
+          context.fillStyle = 'black';
+          context.strokeStyle = 'black';
+          context.font = '30px Arial';
+          context.textAlign = 'center';
+          context.textBaseline = 'middle';
+          context.fillText(text, element.width / 2, element.height / 2);
+          context.strokeText(text, element.width / 2, element.height / 2);
+        }
     }
     
     seputControlListener() {  

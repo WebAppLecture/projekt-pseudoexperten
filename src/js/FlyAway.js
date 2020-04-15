a8f3448c8f800f13a448f20a37d0dac205eebe16
CorinnaGeiselhart <corinna@bayern-mail.de>
2020-04-15 21:18:26 +0200

    Gegner fliegen durch den Raum

--- src/js/FlyAway.js
+++ src/js/FlyAway.js
@@ -1,5 +1,5 @@
 
-import { Ballon } from "./GameObject.js";
+import { Ballon, MovableGameObject } from "./GameObject.js";
 
 export class FlyAway{
 
@@ -7,6 +7,16 @@ export class FlyAway{
         this.start();
         this.setupGameControls(ctx);
         //this.gameLoop();
+
+        //enemies
+        this.ticksToNextStone = 400;
+
+        //enemies 1: destroys the ballon
+        this.enemiesOne = [];
+
+        //enemies 2: ballon grows 
+        this.enemiesTwo =[];
+
     }
 
     start(){
@@ -27,23 +37,6 @@ export class FlyAway{
         }
     }*/
 
-
-    update(ctx){
-        this.player.update(ctx);
-    }
-
-
-    draw(ctx){
-        this.player.draw(ctx);
-        this.drawBoundingBox(ctx);
-    }
-
-    drawBoundingBox(ctx) {
-        ctx.strokeStyle = "#6bd26b";
-        ctx.lineWidth = 2;
-        ctx.strokeRect(10, 10, ctx.canvas.width - 20, ctx.canvas.height - 20);
-    }
-
     tick(ctx) {
         if(this.gameOver) {
             this.gameOverScreen(ctx);
@@ -52,6 +45,26 @@ export class FlyAway{
         this.update(ctx);
         this.draw(ctx);
     }
+
+    update(ctx){
+        this.player.update(ctx);
+        this.updateEnemies(ctx);
+    }
+
+
+    draw(ctx){
+        this.player.draw(ctx);
+        this.drawBoundingBox(ctx);
+        this.enemiesOne.forEach(enemie => enemie.draw(ctx));
+        this.enemiesTwo.forEach(enemie => enemie.draw(ctx));
+    }
+
+    drawBoundingBox(ctx) {
+        ctx.strokeStyle = "#6bd26b";
+        ctx.lineWidth = 2;
+        ctx.strokeRect(10, 10, ctx.canvas.width - 20, ctx.canvas.height - 20);
+    }
+ 
 
     setupGameControls(ctx){
         document.addEventListener("keydown", this.onKeyInteraction.bind(this, true));
@@ -70,6 +83,59 @@ export class FlyAway{
         }
         if(event.keyCode == 40){
             this.player.down(bool);
+        }
+    }
+
+    createEnemies(ctx, enemieArray, color){
+        let radius = 10;
+        let varsToCreateEnemie = this.randomPosition(radius, ctx);
+        
+        enemieArray.push(new MovableGameObject(varsToCreateEnemie[0], varsToCreateEnemie[1], radius, color, 
+            varsToCreateEnemie[2], varsToCreateEnemie[3]));
+    }
+
+    //returns: [positionX, positionY, speedX, speedY] for the enemie
+    randomPosition(radius, ctx){
+        var random_boolean = Math.random() >= 0.5;
+        //on true: Enemie enters the canvas from the top
+        if(random_boolean){
+            return[Math.random() *(ctx.canvas.width - radius - 40) + 40, -radius, 0, Math.random() * (1.5-1) + 1];
+        }
+        //on false: enemie enters the canvas from the left
+        else {
+            return[-radius, Math.random() *(ctx.canvas.width - radius - 40) + 40, Math.random() * (1.5-1) + 1, 0];
+        }
+    }
+
+    updateEnemies(ctx){ 
+        //destroy enemies outside of canvas
+        this.destroyEnemies(ctx, this.enemiesOne);
+        this.destroyEnemies(ctx,this.enemiesTwo);
+
+        this.enemiesOne.forEach(enemie => enemie.update(ctx));
+        this.enemiesTwo.forEach(enemie => enemie.update(ctx));
+        
+        //TODO destroy on collision with canvas
+
+        if(this.ticksToNextStone == 200){
+            this.createEnemies(ctx, this.enemiesOne, "#B22222");
+        } else if (this.ticksToNextStone == 400) {
+            this.createEnemies(ctx, this.enemiesTwo, "#200000");
+        }
+
+
+        this.ticksToNextStone--;
+        if(this.ticksToNextStone == 0){
+            this.ticksToNextStone = 400;
+        }
+        
+    }
+
+    destroyEnemies(ctx, enemiesArray){
+        for(let i = enemiesArray.length; i--;){
+            if(enemiesArray[i].y >= ctx.canvas.height || enemiesArray[i].x >= ctx.canvas.width){
+                enemiesArray.splice(i, 1);
+            }
         }
     }
     
